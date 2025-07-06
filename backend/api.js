1532dd5230115fccd8ec0266c854e75f8c88ea62
Oluwalana Joseph <jossiking234@gmail.com>
2025-07-06 14:48:18 -0700

    Clean frontend: removed backend, fixed main entry for Expo

--- backend/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'http://192.168.43.213:8088', // ⬅️ Change this to your server URL
-});
-
-export default API;
--- backend/middleware/adminAuth.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.user || !req.user.isAdmin) {
-    return res.status(403).json({ message: 'Unauthorized: Admin only' });
-  }
-  next();
-};
